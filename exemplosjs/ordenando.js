d766b1d5c96c7f8747cd5366a0cc955980362503
Cristian Viery <viery.guimaraes01@gmail.com>
2024-05-01 13:24:22 -0300

    Create ordenando.js

---
+++ exemplosjs/ordenando.js
@@ -0,0 +1,26 @@
+
+
+const swap = ()=>{
+
+}
+
+const shuffle = ()=>{
+
+}
+
+const bubble_sort = ()=>{
+
+}
+
+const selection_sort = ()=>{
+    
+}
+
+const quick_sort = ()=>{
+    
+}
+
+const particionamento = ()=>{
+    
+}
+
